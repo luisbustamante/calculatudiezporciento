59773477dc147b6f0a42425d2d1ff3aa087c7a22
Luis Bustamante <luchosrock@gmail.com>
2020-07-27 19:32:42 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NumberFormat from "react-number-format";
+
+import "./assets/main.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-hero-signal">
+      <div className="flex flex-col items-center justify-center min-h-screen py-8">
+        <h1 className="text-6xl">Calcula tu 10%</h1>
+        <div className="flex flex-col items-center justify-center text-6xl">
+          <NumberFormat
+            className="text-center border-2 border-gray-700"
+            thousandSeparator={"."}
+            prefix={"$"}
+            decimalSeparator={","}
+            onValueChange={(values) => {
+              const { value } = values;
+              const saldo = parseFloat(value);
+              if (saldo <= 1000000) {
+                setAmount(saldo);
+              } else if (saldo > 1000000 && saldo <= 10000000) {
+                setAmount(1000000);
+              } else if (saldo > 10000000) {
+                setAmount(Math.min(saldo * 0.1, 4300000));
+              }
+            }}
+          />
+          <div>
+            {Intl.NumberFormat("es-CL", {
+              style: "currency",
+              currency: "CLP",
+            }).format(amount)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

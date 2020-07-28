8a399e9c1c2b35aa641b7b689e912de5e8b37e8f
Luis Bustamante <luchosrock@gmail.com>
2020-07-27 20:26:26 -0400

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import "./assets/main.css";
 function App() {
   const [amount, setAmount] = useState(0);
   return (
-    <div className="App bg-hero-signal">
-      <div className="flex flex-col items-center justify-center min-h-screen py-8">
-        <h1 className="text-6xl">Calcula tu 10%</h1>
-        <div className="flex flex-col items-center justify-center text-6xl">
+    <div className="App px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto text-center">
+      <div className="hero">
+        <div className="hero-headline">
+          <h1 className="text-4xl">Calcula tu 10%</h1>
+        </div>
+        <div className="text-4xl">
           <NumberFormat
-            className="text-center border-2 border-gray-700"
+            className="w-full text-center border-2 border-gray-700"
             thousandSeparator={"."}
             prefix={"$"}
             decimalSeparator={","}
+            placeholder="Fondos disponibles"
             onValueChange={(values) => {
               const { value } = values;
               const saldo = parseFloat(value);
@@ -28,6 +31,7 @@ function App() {
             }}
           />
           <div>
+            <h3>Puedo retirar:</h3>
             {Intl.NumberFormat("es-CL", {
               style: "currency",
               currency: "CLP",
